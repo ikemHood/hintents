d236458c9e8cf09c52b907764848f8230b897ecb
Bug-hunter-x <askzenai@gmail.com>
2026-02-26 09:32:24 +0100

    Fix duplicate LedgerEntryResult after merge

--- internal/rpc/client.go
+++ internal/rpc/client.go
@@ -428,13 +428,6 @@ type GetLedgerEntriesRequest struct {
 	Params  []interface{} `json:"params"`
 }
 
-type LedgerEntryResult = struct {
-	Key                string `json:"key"`
-	Xdr                string `json:"xdr"`
-	LastModifiedLedger int    `json:"lastModifiedLedgerSeq"`
-	LiveUntilLedger    int    `json:"liveUntilLedgerSeq"`
-}
-
 type GetLedgerEntriesResponse struct {
 	Jsonrpc string `json:"jsonrpc"`
 	ID      int    `json:"id"`
